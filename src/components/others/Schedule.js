429758653631f024a9b826d14b619d4900604508
Choridah Al-Bahiyyah <bahaque03@gmail.com>
2022-07-31 11:13:19 +0700

    create modal & slide klausa - but still problem

--- src/components/others/Schedule.js
+++ src/components/others/Schedule.js
@@ -1,64 +1,12 @@
 import React from 'react'
-import { CgProfile } from "react-icons/cg";
+import Navbar from "../navbar/Navbar";
+import CarouselFadeExample from "../carousel/Klausa";
 
 const Schedule = () => {
   return (
     <div>
     {/* ---------------- Navbar -----------------------*/}
-      <nav class="navbar is-white-light" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a role="button" href="/schedule:id" className="navbar-burger burger mt-2" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <div><CgProfile style={{color: '#FDCB00', fontSize: '45px'}}/></div>
-        </a>
-      </div>
-    
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <a href="/" class="navbar-item">
-            Home
-          </a>
-    
-          <a href="/" class="navbar-item">
-            Documentation
-          </a>
-    
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a href="/" class="navbar-link">
-              More
-            </a>
-    
-            <div class="navbar-dropdown">
-              <a href="/" class="navbar-item">
-                About
-              </a>
-              <a href="/" class="navbar-item">
-                Jobs
-              </a>
-              <a href="/" class="navbar-item">
-                Contact
-              </a>
-              <hr class="navbar-divider"/>
-              <a href="/" class="navbar-item">
-                Report an issue
-              </a>
-            </div>
-          </div>
-        </div>
-    
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a href="/" class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a href="/" class="button is-light">
-                Log in
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     {/* ---------------- Body -----------------------*/}
     <div className="container mt-5 mx-5">
@@ -69,12 +17,13 @@ const Schedule = () => {
         {/* --------------- Slide Klausa ------------- */}
         <div className="mt-5 mb-5">
           <span className="button is-warning mr-2"></span>
-          <span className="button is-warning mr-1">1</span>
-          <span className="button is-warning mr-1">2</span>
-          <span className="button is-warning mr-1">3</span>
-          <span className="button is-warning mr-1">4</span>
-          <span className="button is-warning mr-2">5</span>
+          <span className="btn-list-klausa mr-1">1</span>
+          <span className="btn-list-klausa mr-1">2</span>
+          <span className="btn-list-klausa mr-1">3</span>
+          <span className="btn-list-klausa mr-1">4</span>
+          <span className="btn-list-klausa mr-2">5</span>
           <span className="button is-warning mr-1"></span>
+          <CarouselFadeExample />
         </div>
         
         {/* --------------- Assessment ------------- */}
--- src/components/template/Dashboard.js
+++ src/components/template/Dashboard.js
@@ -1,71 +1,19 @@
 import React from 'react'
-import { CgProfile } from "react-icons/cg";
+import Navbar from "../navbar/Navbar";
 import { MdOutlineManageSearch } from "react-icons/md";
 import { FiDownloadCloud } from "react-icons/fi";
+import App from "../modal/CreateSchedule";
 
 const Dashboard = () => {
   return (
     <div>
     {/* ---------------- Navbar -----------------------*/}
-      <nav className="navbar is-white-light" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a role="button" href="/dashboard" className="navbar-burger burger mt-2" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <div><CgProfile style={{color: '#FDCB00', fontSize: '45px'}}/></div>
-        </a>
-      </div>
-    
-      <div id="navbarBasicExample" href="/" className="navbar-menu">
-        <div href="/" className="navbar-start">
-          <a href="/" className="navbar-item">
-            Home
-          </a>
-    
-          <a href="/" className="navbar-item">
-            Documentation
-          </a>
-    
-          <div href="/" className="navbar-item has-dropdown is-hoverable">
-            <a href="/" className="navbar-link">
-              More
-            </a>
-    
-            <div href="/" className="navbar-dropdown">
-              <a href="/" className="navbar-item">
-                About
-              </a>
-              <a href="/" className="navbar-item">
-                Jobs
-              </a>
-              <a href="/" className="navbar-item">
-                Contact
-              </a>
-              <hr href="/" className="navbar-divider"/>
-              <a href="/" className="navbar-item">
-                Report an issue
-              </a>
-            </div>
-          </div>
-        </div>
-    
-        <div href="/" className="navbar-end">
-          <div href="/" className="navbar-item">
-            <div href="/" className="buttons">
-              <a href="/" className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a href="/" className="button is-light">
-                Log in
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     {/* ---------------- Body -----------------------*/}
     <div className="container mt-5 mx-5">
       {/* --------------- Create Schedule ------------- */}
-      <a href="/" className="btn-input btn-fullwidth text-bold">Create Schedule</a>
+      <App />
       
       {/* --------------- Search ------------- */}
       <form className="my-5 col-2-search mr-3">
